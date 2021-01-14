dd120566b475effd9fe07830ebbec0d0920aa364
Mark Walker <markwal@hotmail.com>
2021-01-14 01:06:59 -0800

    Open external links in a browser window
    Also attempt to minimize the white flash in dark mode (doesn't really
    work though)

--- src/electron/electron.js
+++ src/electron/electron.js
@@ -1,18 +1,54 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, nativeTheme, shell } = require('electron')
 const path = require('path')
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    backgroundColor: (nativeTheme.shouldUseDarkColors ? '#000' : '#FFF'),
     webPreferences: {
       contextIsolation: true
     }
   })
 
   win.loadURL(`file://${path.join(__dirname, 'index.html')}`)
- 
-  win.webContents.openDevTools()
+
+  win.webContents.on('new-window', function(e, url) {
+    u = new URL(url)
+    console.log("new-window u.hostname ", u.hostname)
+    console.log("new-window u.pathname ", u.pathname)
+    console.log("new-window u.protocol ", u.protocol)
+    e.preventDefault()
+    shell.openExternal(url)
+  })
+
+  win.webContents.on('will-navigate', function(e, url) {
+    console.log("on will-navigate")
+
+    u = new URL(url)
+    console.log("u.hostname ", u.hostname)
+    console.log("u.pathname ", u.pathname)
+    console.log("u.protocol ", u.protocol)
+    if (u.protocol === 'http:' || u.protocol === 'https:') {
+      e.preventDefault()
+      shell.openExternal(url)
+    }
+  })
+
+  /* FUTURE when on 'new-window' is deprecated
+  win.webContents.setWindowOpenHandler(function({ url }) {
+    u = new URL(url)
+    if (u.protocol === 'http:' || u.protocol === 'https:') {
+      shell.openExternal(url)
+      return { action: 'deny' }
+    }
+    return { action: 'allow', overrideBrowserWindowOptions: { modal: true } }
+  });
+  */
+
+  if (!app.isPackaged) {
+    win.webContents.openDevTools()
+  }
 }
 
 app.whenReady().then(createWindow)
@@ -28,5 +64,3 @@ app.on('activate', () => {
     createWindow()
   }
 })
-
-
