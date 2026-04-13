b04bbd27368e70a0d143f3d7c4a8dc4879a748de
Mark Walker <markwal@hotmail.com>
2026-04-12 20:04:58 -0700

    Update electron

--- src/electron/electronmain.js
+++ src/electron/electronmain.js
@@ -9,6 +9,11 @@ const path = require('path')
 
 let electronWindow;
 
+function isExternalHttpUrl(url) {
+  const parsed = new URL(url);
+  return parsed.protocol === 'http:' || parsed.protocol === 'https:';
+}
+
 function createWindow() {
   electronWindow = new BrowserWindow({
     width: app.isPackaged ? 800 : 1200,
@@ -24,44 +29,31 @@ function createWindow() {
 
   electronWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 
-  electronWindow.webContents.on('new-window', function(e, url) {
-    u = new URL(url)
-    console.log("new-window u.hostname ", u.hostname)
-    console.log("new-window u.pathname ", u.pathname)
-    console.log("new-window u.protocol ", u.protocol)
-    e.preventDefault()
-    shell.openExternal(url)
-  })
+  electronWindow.webContents.setWindowOpenHandler(({ url }) => {
+    if (isExternalHttpUrl(url)) {
+      shell.openExternal(url)
+      return { action: 'deny' }
+    }
+
+    return { action: 'allow' }
+  });
 
   electronWindow.webContents.on('will-navigate', function(e, url) {
-    console.log("on will-navigate")
-
-    u = new URL(url)
-    console.log("u.hostname ", u.hostname)
-    console.log("u.pathname ", u.pathname)
-    console.log("u.protocol ", u.protocol)
-    if (u.protocol === 'http:' || u.protocol === 'https:') {
+    if (isExternalHttpUrl(url)) {
       e.preventDefault()
       shell.openExternal(url)
     }
   })
 
-  /* FUTURE when on 'new-window' is deprecated
-  electronWindow.webContents.setWindowOpenHandler(function({ url }) {
-    u = new URL(url)
-    if (u.protocol === 'http:' || u.protocol === 'https:') {
-      shell.openExternal(url)
-      return { action: 'deny' }
-    }
-    return { action: 'allow', overrideBrowserWindowOptions: { modal: true } }
-  });
-  */
-
   // watch for maximize state change
   electronWindow.on('maximize', () => { electronWindow.webContents.send('maximize') });
   electronWindow.on('unmaximize', () => { electronWindow.webContents.send('unmaximize') });
   // trigger one now for initial state
-  electronWindow.webContents.send(electronWindow.isMaximized ? "maximize" : "unmaximize")
+  electronWindow.webContents.once('did-finish-load', () => {
+    electronWindow.webContents.send(
+      electronWindow.isMaximized() ? 'maximize' : 'unmaximize'
+    );
+  });
 
   if (!app.isPackaged) {
     electronWindow.webContents.openDevTools()
--- src/js/components/ZMachine.js
+++ src/js/components/ZMachine.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import SplitPane from 'react-split-pane';
+import { SplitPane } from 'react-split-pane';
 
 import ModalController from './ModalController';
 import Settings from './Settings';
